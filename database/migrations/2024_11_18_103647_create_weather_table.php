4408eadf630d5ae087c24feb29d2cc12d99f5600
alden <connect@alenev.name>
2024-11-18 14:06:25 +0200

    added Weather DB-table migration

---
+++ database/migrations/2024_11_18_103647_create_weather_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weather', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('timestamp_dt')->nullable();
+            $table->string('city_name');
+            $table->decimal('min_tmp', total: 4, places: 2);
+            $table->decimal('max_tmp', total: 4, places: 2);
+            $table->decimal('wind_spd', total: 4, places: 2); #may be 10.00+ ?
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weather');
+    }
+};
