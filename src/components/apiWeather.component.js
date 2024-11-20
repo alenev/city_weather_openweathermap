1c64b8dcbfa9dbec0a42c1d1af347db0ad3f7889
alden <connect@alenev.name>
2024-11-20 10:18:08 +0200

    complete dbWeather.component.js

--- src/components/apiWeather.component.js
+++ src/components/apiWeather.component.js
@@ -7,12 +7,10 @@ import axios from 'axios';
 import Swal from 'sweetalert2'
 import moment from 'moment';
 
-function getFormattedDates(weatherItems) {
-    const firstTimestamp = weatherItems[0]?.timestamp_dt;
-    const lastTimestamp = weatherItems[weatherItems.length - 1]?.timestamp_dt;
-    const firstDate = moment(firstTimestamp * 1000).utc().format('YYYY-MM-DD, HH:mm:ss A');
-    const lastDate = moment(lastTimestamp * 1000).utc().format('YYYY-MM-DD, HH:mm:ss A');
-    return { firstDate, lastDate };
+
+function getFormattedTimestamp(timestamp) {
+    const formatTimestamp = moment(timestamp * 1000).utc().format('YYYY-MM-DD, HH:mm:ss A');
+    return formatTimestamp;
 }
 
 export default function ApiWeather({inputValue}){
@@ -28,10 +26,10 @@ export default function ApiWeather({inputValue}){
             const response = await axios.get(`http://127.0.0.1:8000/api/weather/get_open_weather_map_city_data?city_name=${inputValue}`); 
             setweatherItems(response.data.data);
         } catch (error) {
-            setError(error.message);
+            setError(error.response.data.message);
             Swal.fire({
                 icon:"error",
-                text:error.message
+                text:error.response.data.message
             })
         } finally {
             setIsLoading(false);
@@ -66,10 +64,10 @@ export default function ApiWeather({inputValue}){
                 text:response.data.message
             })
         } catch (error) {
-            setError(error.message);
+            setError(error.response.data.message);
             Swal.fire({
                 icon:"warning",
-                text:"Saving forecast error"
+                text:error.response.data.message
             })
         } finally {
             setIsLoading(false);
@@ -80,15 +78,15 @@ export default function ApiWeather({inputValue}){
         <div>
         {isLoading && (
             <div className="m-2 row d-flex justify-content-center"> 
-                <div class="spinner-border text-warning" role="status">
-                    <span class="visually-hidden">Loading...</span>
+                <div className="spinner-border text-warning" role="status">
+                    <span className="visually-hidden">Loading...</span>
                 </div>
             </div>
         )}
 		{error && (
             <div className="m-2 row d-flex justify-content-center text-danger">Error: {error}</div>
         )}
-        {weatherItems.length > 0 && (
+        {weatherItems.length > 1 && (
             <>
             <Container className="mt-3 p-3 border border-1">
                 <Row>
@@ -103,12 +101,13 @@ export default function ApiWeather({inputValue}){
                 </Row>
                 <Row>
                     <Col md={12} className="city_weather_period_info mt-1">
-                        Starts at: {getFormattedDates(weatherItems).firstDate}
+                        Starts at: {getFormattedTimestamp(weatherItems[0].timestamp_dt)} UTC
+
                     </Col>
                 </Row>
                 <Row>
                     <Col md={12} className="city_weather_period_info mt-1">
-                        Ends at: {getFormattedDates(weatherItems).lastDate}
+                        Ends at: {getFormattedTimestamp(weatherItems[weatherItems.length - 1].timestamp_dt)} UTC
                     </Col>
                 </Row>
                 <Row>
@@ -121,19 +120,19 @@ export default function ApiWeather({inputValue}){
             <table className="table table-bordered mb-5 text-center">
                 <thead className="text-primary fs-6">
                 <tr>
-                    <th className="text-primary">Datetime</th>
-                    <th className="text-primary">Min temp</th>
-                    <th className="text-primary">Max temp</th>
-                    <th className="text-primary">Wind speed</th>
+                    <th className="text-primary text-start">Datetime</th>
+                    <th className="text-primary text-start">Min temp</th>
+                    <th className="text-primary text-start">Max temp</th>
+                    <th className="text-primary text-start">Wind speed</th>
                 </tr>
                 </thead>
                 <tbody>
-                    {weatherItems.map((city, key) => (                      
-                        <tr className="city_weather_row" key={key}>
-                        <td>{city.timestamp_dt}</td>
-                        <td>{city.min_tmp}</td>
-                        <td>{city.max_tmp}</td>
-                        <td>{city.wind_spd}</td>
+                    {weatherItems.map((weatherItem, key) => (                      
+                        <tr className="city_weather_row text-start" key={key}>
+                        <td className="text-start">{getFormattedTimestamp(weatherItem.timestamp_dt)}</td>
+                        <td className="text-start">{weatherItem.min_tmp}</td>
+                        <td className="text-start">{weatherItem.max_tmp}</td>
+                        <td className="text-start">{weatherItem.wind_spd}</td>
                         </tr>
                     ))}
                 </tbody>
--- src/components/dbWeather.component.js
+++ src/components/dbWeather.component.js
@@ -1,9 +1,103 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import Swal from 'sweetalert2'
+import moment from 'moment';
 
+function getFormattedDates(dateTime) {
+    const momentDate = moment(dateTime);
+    const formatDate = momentDate.utc().format('YYYY-MM-DD, HH:mm:ss A');
+    return formatDate;
+}
+function getFormattedTimestamp(timestamp) {
+    const formatTimestamp = moment(timestamp * 1000).utc().format('YYYY-MM-DD, HH:mm:ss A');
+    return formatTimestamp;
+}
 
-export default function DbWeather(){
+export default function DbWeather({inputValue}){
+    const [weatherItem, setweatherItem] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            setError(null); 
+        try {
+            const response = await axios.get(`http://127.0.0.1:8000/api/weather/get_by_city_name?city_name=${inputValue}`); 
+            setweatherItem(response.data.data);
+        } catch (error) {
+            setError(error.response.data.message);
+            console.log("error", error);
+            Swal.fire({
+                icon:"error",
+                text:error.response.data.message
+            })
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    if (inputValue) {
+        fetchData();
+    }else{
+        Swal.fire({
+            icon:"warning",
+            text:"City name is empty"
+        })
+    }
+    }, [inputValue]);
+
+    return (
+        <div>
+        {isLoading && (
+            <div className="m-2 row d-flex justify-content-center"> 
+                <div className="spinner-border text-warning" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        )}
+		{error && (
+            <div className="m-2 row d-flex justify-content-center text-danger">Error: {error}</div>
+        )}
+        {weatherItem && (
+            <>
+            <Container className="mt-3 p-3 border border-1">
+                <Row>
+                    <Col md={12} className="fs-3 fw-bold">
+                        {weatherItem.city_name}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12} className="fs-6">
+                        Updated at: {getFormattedDates(weatherItem.updated_at)} UTC
+                    </Col>
+                </Row>                 
+            </Container>
+
+            <table className="table table-bordered mb-5 text-center">
+                <thead className="text-primary fs-6">
+                <tr>
+                    <th className="text-primary text-start">Datetime</th>
+                    <th className="text-primary text-start">Min temp</th>
+                    <th className="text-primary text-start">Max temp</th>
+                    <th className="text-primary text-start">Wind speed</th>
+                </tr>
+                </thead>
+                <tbody>                 
+                        <tr className="city_weather_row">
+                        <td className="text-start">{getFormattedTimestamp(weatherItem.timestamp_dt)}</td>
+                        <td className="text-start">{weatherItem.min_tmp}</td>
+                        <td className="text-start">{weatherItem.max_tmp}</td>
+                        <td className="text-start">{weatherItem.wind_spd}</td>
+                        </tr>
+                </tbody>
+            </table>
+            </>
+            )}
+    </div>
+    );
 }
